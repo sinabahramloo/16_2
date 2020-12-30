376da0a2de0f1b0a7da9ebccf3c65477027677ca
sina <sinabahramloo@gmail.com>
2020-12-30 15:27:53 +0330

    Add Employee model to project

---
+++ src/main/java/Model/Employee.java
@@ -0,0 +1,61 @@
+package Model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "mk_Employee")
+public class Employee {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "empCode")
+    private Integer empCode;
+    @Column(name = "salary")
+    private Double salary;
+    @OneToMany(mappedBy = "employee",cascade = {CascadeType.ALL})
+    private Set<Address> addresses;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmpCode() {
+        return empCode;
+    }
+
+    public void setEmpCode(Integer empCode) {
+        this.empCode = empCode;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+}
