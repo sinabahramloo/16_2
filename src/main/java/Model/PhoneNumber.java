3d663c7d19be0b81173ed2c18c99e94b000c5b86
sina <sinabahramloo@gmail.com>
2020-12-30 15:29:24 +0330

    edit PhoneNumber.java model and add setter and getter

--- src/main/java/Model/PhoneNumber.java
+++ src/main/java/Model/PhoneNumber.java
@@ -16,4 +16,36 @@ public class PhoneNumber {
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name = "fk_address",nullable = false)
     private Address address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getMobNumber() {
+        return mobNumber;
+    }
+
+    public void setMobNumber(String mobNumber) {
+        this.mobNumber = mobNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
