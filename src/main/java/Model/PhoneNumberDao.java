ed883701d46ff1df852a984b7a20381d128939e9
sina <sinabahramloo@gmail.com>
2020-12-30 15:38:51 +0330

    Add PhoneNumberDao.java to project it's an implementation of Generic Dao

---
+++ src/main/java/Model/PhoneNumberDao.java
@@ -0,0 +1,16 @@
+package Model;
+
+
+import javax.persistence.EntityManager;
+
+public class PhoneNumberDao extends GenericDao<PhoneNumber, Integer> {
+    public PhoneNumberDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Override
+    public Class<PhoneNumber> getentityClass() {
+        return PhoneNumber.class;
+    }
+
+}
